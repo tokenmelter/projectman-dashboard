531db26785557befccb2e160cda3f6f08365c9f6
tokenmelter <tokenmelter@gmail.com>
2026-02-24 18:44:31 -0800

    Remove auto-refresh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta httpEquiv="refresh" content="60" />
-      </head>
       <body>{children}</body>
     </html>
   );
